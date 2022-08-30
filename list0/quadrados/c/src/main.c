df0026ecc82d207a8ecba4b8907fc4dc86a11957
i3elj <bercarrogui@gmail.com>
2022-08-30 01:04:16 -0300

    added problem header

--- list0/quadrados/c/src/main.c
+++ list0/quadrados/c/src/main.c
@@ -1,3 +1,17 @@
+/*
+ * QUADRAD2 - Quadrados (Squares)
+ *    Given an integer N, determine how much is N square.
+ *
+ *  Inputs:
+ *    - The input is made by only one test case with just one line which contains the integer N.
+ *  Outputs:
+ *    - This program must produce just only one line containing the value of N square
+ *  Restrictions:
+ *    - |N| <= 10.000
+ *
+ * Obs: use make to run this program
+*/
+
 #include <stdio.h>
 #include "../lib/func.h"
 
